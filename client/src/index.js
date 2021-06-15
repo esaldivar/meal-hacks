3c9001fe9a8ef0ac0c9e307d3a8bafe3d52a17dc
Eric Saldivar <Eric@samlas-mbp.lan>
2021-06-14 21:05:40 -0500

    getting ready for htmlwebpack plugin

--- client/src/index.js
+++ client/src/index.js
@@ -16,16 +16,18 @@ import Icon from '../assets/logo.png';
 
 function component() {
   const element = document.createElement('div');
-
+  const btn = document.createElement('button'); 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['goodbye', 'webpack'], ' ');
   element.classList.add('hello');
 
-   // Add the image to our existing div.
-   const myIcon = new Image();
-   myIcon.src = Icon;
- 
-   element.appendChild(myIcon);
+   // testing a button
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+   
+  
   
   return element;
 }
