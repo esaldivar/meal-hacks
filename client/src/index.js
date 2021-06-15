6680dbad7812f083329c3c5c8e010c6712312302
Eric Saldivar <Eric@samlas-mbp.lan>
2021-06-14 19:43:15 -0500

    pushing to main

--- client/src/index.js
+++ client/src/index.js
@@ -16,7 +16,7 @@ function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['goodbye', 'webpack'], ' ');
 
   return element;
 }
