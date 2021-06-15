058b268c8ebee266ec3f06281b5b4b207a0a0b59
Eric Saldivar <Eric@samlas-mbp.lan>
2021-06-15 12:29:37 -0500

    creating navbar, having trouble with the image icon

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -9,7 +9,8 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import Icon from '@material-ui/core/Icon';
+import Avatar from '@material-ui/core/Avatar';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -22,6 +23,13 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  login: {
+    border: "solid white 0.1rem",
+  },
+  keys: {
+    color: 'gold',
+    margin: '0 5px 0 0'
+  }
 }));
 
 const NavBar = () => {
@@ -32,12 +40,13 @@ const NavBar = () => {
       <AppBar position="static">
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
+          <Avatar alt="Meal Hacks Icon" src="logo.png" />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
+          <Button color="inherit">Meal Hacks</Button>
+          <Button color="inherit">Find a Recipe</Button>
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit" className={classes.login}><Icon className={classes.keys}>cake</Icon>Login</Button>
         </Toolbar>
       </AppBar>
     </div>
