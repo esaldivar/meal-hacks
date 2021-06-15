cf19a4432e34e557a83eaf0e3155c2c6e1b840ba
Eric Saldivar <Eric@samlas-mbp.lan>
2021-06-14 20:11:26 -0500

    server boilerplate set up.  Server is running.  Need to reroute it to the front End

--- server/server.js
+++ server/server.js
@@ -1,1 +1,12 @@
-console.log('bonjour elliot')
+const express = require('express');
+
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('Bonjour Elliot');
+});
+
+app.listen(3000, ()=> {
+    console.log('server started on port 3000');
+});
