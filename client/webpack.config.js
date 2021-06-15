0afd76833d2f542e37467efa5f6c11288278b569
Eric Saldivar <Eric@samlas-mbp.lan>
2021-06-14 21:43:18 -0500

    frontend webdev talking to server

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
+    clean: true,
+    publicPath: '/'
   },
   devtool: 'inline-source-map',
   plugins: [
