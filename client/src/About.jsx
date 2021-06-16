0ead35553c4330f744afd386acc2d4bae13cace3
Eric Saldivar <Eric@samlas-mbp.lan>
2021-06-16 11:28:26 -0500

    about page nearly complete -- auto type effect

--- client/src/About.jsx
+++ client/src/About.jsx
@@ -18,11 +18,39 @@
  
  const About = () => {
    const classes = useStyles();
+
+   const el = React.useRef(null);
+   // Create reference to store the Typed instance itself
+     const typed = React.useRef(null);
  
+   React.useEffect(() => {
+     const options = {
+         strings: [
+         'Are you hungry?',
+         'Not sure what to cook?',
+         'Meal hacks has got you covered!'
+       ],
+       typeSpeed: 50,
+       backSpeed: 50,
+     };
+     
+     // elRef refers to the <span> rendered below
+     typed.current = new Typed(el.current, options);
+     
+     return () => {
+       // Make sure to destroy Typed instance during cleanup
+       // to prevent memory leaks
+       typed.current.destroy();
+     }
+   }, [])
+
    return (
-     <div className="funnyText">
-         <h1>Meal Hacks</h1>
-         
+    <div>
+        <div className="funnyText">
+            <h1>Meal Hacks</h1>
+            <h2 className="typeBox" ref={el}/>
+        </div>
+           
      </div>
    );
  }
