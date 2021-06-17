c71ed0b69bc6fd0378dd875937f352a25e9778b8
Eric Saldivar <Eric@samlas-mbp.lan>
2021-06-17 14:53:25 -0500

    using passport, bcrypt, and mongoose to make a login server

--- client/src/Recipe.jsx
+++ client/src/Recipe.jsx
@@ -1,26 +1,2 @@
-/**
- * 
- */
 
- import React from 'react';
- import { makeStyles } from '@material-ui/core/styles';
- import {Link} from 'react-router-dom';
- 
- 
- 
- const useStyles = makeStyles((theme) => ({
-   root: {
-    
-   },
- }));
- 
-const Recipe = () => {
-   const classes = useStyles();
- 
-   return (
-     
-   );
- }
- 
- export default Recipe;
  
