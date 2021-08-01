a58eefb8ec9dc75384d96c542639c9f07ac37b70
Eric Saldivar <Eric@samlas-mbp.lan>
2021-07-31 21:13:58 -0500

    set up auth routes

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema(
 );
 
 
-const User = mongoose.model('users\'', userSchema);
+const User = mongoose.model('users', userSchema);
 
 module.exports = {
   User
