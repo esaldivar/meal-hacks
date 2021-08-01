15b25fdc32562a4cfae26c32fdfa7dcdb579f149
Eric Saldivar <Eric@samlas-mbp.lan>
2021-07-31 20:36:01 -0500

    front and backend fixed for webpack config

--- server/server.js
+++ server/server.js
@@ -1,52 +1,42 @@
 const path = require('path');
 const express = require('express');
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const mongoose = require('mongoose');
-
-
 const app = express();
-const config = require('../webpack.config');
-const compiler = webpack(config);
+const PORT = 3000;
 
-// Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// configuration file as a base.
-
-//body parser
+// const cors = require('cors')
 app.use(express.json());
-app.use(express.urlencoded({
-	extended:true
-}));
-
-// //cors
-// app.use(cors({
-// 	origin:'http://localhost:3000', //<== location of the react app were connecting to
-// 	credentials: true
-// }));
 
 
-//Routes
-app.post('/login', (req,res)=> {
-  console.log(req.body)
-})
+app.use(express.urlencoded({extended:true}));
 
-app.post('/register', (req,res)=> {
-  console.log(req.body)
-})
+// Route Handlers
+// app.use('/api', apiRouter);
 
-app.post('/user', (req,res)=> {
-  res.send('Hello, from the back end')
-})
+//Default Error Handler
 
-
-
-app.use(
-	webpackDevMiddleware(compiler, {
-		publicPath: config.output.publicPath,
-	})
-);
-
-// Serve the files on port 3000.
-app.listen(3000, function () {
-	console.log('Example app listening on port 3000!\n');
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
 });
+
+// statically serve everything in the build folder on the route '/build'
+app.use('/dist', express.static(path.join(__dirname, '../dist')));
+// serve index.html on the route '/'
+app.get('/', (req, res) => {
+  return res.sendFile(path.join(__dirname, '../index.html'));
+});
+
+// Catch-all to unknown routes (404)
+app.use((req,res) => res.status(404).send('not found'))
+//Start Server
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = app;
